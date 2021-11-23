fb1ffa5bf807c864827eb5fffa03b24348f46f19
Evelyn Nguyen <enguyen6208@gmail.com>
2021-11-22 17:08:35 -0800

    finished header and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <h4>&copy; 2021 Evelyn Nguyen</h4>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Evelyn Nguyen</h1>
+        <ul>
+            <li><a href="#about-me">About Me</a></li>
+            <li><a href="#work">Work</a></li>
+            <li><a href="#contact-me">Contact Me</a></li>
+            <li><a href="#resume">Resume</a></li>
+        </ul>
+    </header>
+  );
+}
+
+export default Header;
