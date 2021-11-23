90e7e8b5a1aa95b61453dd5912984ca489a40308
Evelyn Nguyen <enguyen6208@gmail.com>
2021-11-22 16:58:02 -0800

    set up react app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from "./components/Header"
+import Banner from "./components/Banner"
+import Footer from "./components/Footer"
+import AboutMe from "./components/About-Me"
+import Work from "./components/Work"
+import Contact from "./components/Contact"
+import Resume from "./components/Resume"
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Banner />
+    <AboutMe />
+    <Work />
+    <Contact />
+    <Resume />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
