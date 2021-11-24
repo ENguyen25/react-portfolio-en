3609604ea5ea08d089270e8a6c2057b9fbff0977
Evelyn Nguyen <enguyen6208@gmail.com>
2021-11-24 14:32:13 -0800

    added redirecting url and fontface

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Cards(props) {
     return (
-        <div className="content-cards" id={props.id}>
+        <div className="content-cards" id={props.id} onClick={() => {
+            window.location.href = `${props.url}`
+            return null
+            }}>
             <div className="subtitle">
                 <h3>{props.title}</h3>
                 <p>{props.language}</p>
