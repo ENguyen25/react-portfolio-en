2f150d8fa97451b65feb2f737e7cdb6e86d3db5e
Evelyn Nguyen <enguyen6208@gmail.com>
2021-11-24 14:39:29 -0800

    edited bio

--- src/components/About-Me.js
+++ src/components/About-Me.js
@@ -7,9 +7,8 @@ function AboutMe() {
         <h2 className="heading">About Me</h2>
         <div className="content">
             <img id="headshot" src={headshot} alt="Evelyn's profile"/>
-            <p>Hey there! I am a graduate of University of California, Davis with double Bachelors of Arts in Sociology-Organizational Studies and Psychology.
-                <br/><br/>I am an aspiring social entrepreneur with a passion for creating products and establishing manufacturing processes that reduce environmental and social harm. I am in the process of producing fashionable items, such as tote bags and purses, using second-hand fabric.
-                <br/><br/>My philosophy of business leadership is mindfulness of people and the environment. I believe that our greatest resources are the people who work for a company, from the bottom to the top, and everyone should be treated with equal respect and dignity regardless of their position in the hierarchy. I also believe that we should give back to our Earth as much as we take from Her. I strive to practice all aspects of this philosophy through my company.
+            <p id="bio">Hey there! I am a Full-Stack Web Developer with experience in JavaScript frameworks, front-end frameworks, and responsive design. I am interested in creating aesthetically pleasing, dynamic websites that appeal to user needs.
+                <br/><br/>My passions lie in developing technology for social good and providing more resources for marginalized communities. As an aspiring social entrepreneur, I hope to contribute to the development of technology and companies that better our society.
             </p>
         </div>
         </section>
