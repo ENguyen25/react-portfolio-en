d200deac03dc186118c384fd75609bbebf4ec2ef
Evelyn Nguyen <enguyen6208@gmail.com>
2022-02-08 23:04:45 -0800

    re-design work page

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import Cards from './Cards';
-import { Link } from 'react-router-dom'
+import Ubiquitous from "./pages/Ubiquitous"
+import Friendship from "./pages/Friendship"
+import Blog from "./pages/Blog"
+import Weather from "./pages/Weather"
+import Quiz from "./pages/Quiz"
 
 function Work() {
     return (
-        <section className="container" id="work">
-        <h2 className="heading">Work</h2>
-        <div className="content">
-            <div className="work-cards">
-                <Cards id='ubiquitous' title='Ubiquitous Happiness' language='JavaScript, Sequelize, Handlebars' url='/ubiquitous'/>
-                <Cards id='friendship' title='Know More Tiers' language='ReactJS, GraphQL, SASS' url='/friendship'/>
-                <Cards id='blog' title='Tech Blog' language='JavaScript, Sequelize, Handlebars' url='/blog'/>
-                <Cards id='weather' title='Weather App' language='JavaScript, HTML, CSS' url='/weather'/>
-                <Cards id='quiz' title='Coding Quiz' language='JavaScript, HTML, CSS' url='/quiz'/>
-            </div>
+        <div>
+            <Ubiquitous />
+            <Friendship />
+            <Blog />
+            <Weather />
+            <Quiz />
         </div>
-        </section>
     )
 }
 
--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from '../../images/TechBlog_Logo.png'
 
 export default function Blog() {
     return (
         <section className='projectContainer'>
             <h2>Technology for Social Good Blog</h2>
             <div className='projectContent'>
+                <img id="project-photo" src={logo} alt="Tech Blog"></img>
                 <div className='projectText'>
                     <p>As someone who is passionate about writing and using technology to make the world a better place, I developed a blog where I can post content about technology for social good. The blog features user authentication, as well as user ability to create a dated blog post, comment on other posts, delete and update posts.</p>
                     <br></br>
